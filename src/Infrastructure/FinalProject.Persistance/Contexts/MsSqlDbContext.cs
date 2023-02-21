b34d140890aab30fe55a33a489898ba24ea4cf70
Bedirhan Şık <Bedirhan_98@outlook.com>
2023-02-22 00:00:24 +0300

    mssql connected

---
+++ src/Infrastructure/FinalProject.Persistance/Contexts/MsSqlDbContext.cs
@@ -0,0 +1,33 @@
+﻿using FinalProject.Domain.Entities;
+using FinalProject.Domain.Entities.Common;
+using FinalProject.Domain.Entities.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalProject.Persistence.Contexts
+{
+    public class MsSqlDbContext : IdentityDbContext<AppUser, AppRole, string>
+    {
+        public MsSqlDbContext(DbContextOptions<MsSqlDbContext> options) : base(options)
+        {
+        }
+        public DbSet<ShopList> ShopLists { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            var Datas = ChangeTracker.Entries<BaseEntity>();
+            foreach (var data in Datas)
+            {
+                if (data.State == EntityState.Added)
+                    data.Entity.CreationDate = DateTime.UtcNow;
+
+                if (data.State == EntityState.Modified)
+                    data.Entity.UpdateDate = DateTime.UtcNow;
+            }
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+    }
+}
---
+++ src/Infrastructure/FinalProject.Persistance/PersistenceServiceRegistration.cs
@@ -0,0 +1,30 @@
+﻿using FinalProject.Domain.Entities.Identity;
+using FinalProject.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FinalProject.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services, IConfiguration configuration, string enviroment = "Development")
+        {
+
+            services.AddDbContext<MsSqlDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("MsSqlConnection")), ServiceLifetime.Singleton);
+
+
+            services.AddIdentity<AppUser, AppRole>(options =>
+            {
+                options.Password.RequiredLength = 3;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+            }).AddEntityFrameworkStores<MsSqlDbContext>();
+
+
+        }
+    }
+}
