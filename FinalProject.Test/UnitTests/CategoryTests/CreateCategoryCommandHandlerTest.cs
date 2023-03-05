065bbbcecf2e2a4cdca53dbb8f6cd7334e6a4325
Bedirhan Şık <Bedirhan_98@outlook.com>
2023-03-05 18:23:16 +0300

    CreateCategory unit test added

--- FinalProject.Test/UnitTests/CategoryTests/CreateCategoryCommandHandlerTest.cs
+++ FinalProject.Test/UnitTests/CategoryTests/CreateCategoryCommandHandlerTest.cs
@@ -1,63 +1,62 @@
-﻿//using FinalProject.Application.Features.CategoryFeatures.Commands.CreateCategory;
-//using FinalProject.Application.Interfaces.Repositories.CategoryRepositories;
-//using FinalProject.Application.Wrappers.Responses;
-//using FinalProject.Domain.Entities;
-//using Moq;
-//using Xunit;
+﻿using FinalProject.Application.DTOs.Category;
+using FinalProject.Application.Features.CategoryFeatures.Commands.CreateCategory;
+using FinalProject.Application.Interfaces.Services.CategoryService;
+using FinalProject.Application.Wrappers.Base;
+using FinalProject.Domain.Entities;
+using Moq;
+using Xunit;
 
-//namespace FinalProject.Test.UnitTests.CategoryTests
-//{
-//    public class CreateCategoryCommandHandlerTest
-//    {
-//        [Fact]
-//        public async void CreateCategory_IsSuccess()
-//        {
-//            CreateCategoryCommandRequest categoryRequest = new CreateCategoryCommandRequest()
-//            {
-//                Name = "TestCategory",
-//                ShopListId = Guid.NewGuid(),
-//            };
-//            Category category = new Category()
-//            {
-//                Id = Guid.NewGuid(),
-//                Name = "asdf",
-//                ShopListId = categoryRequest.ShopListId,
-//                CreationDate = DateTime.Now,
-//                UpdateDate = DateTime.Now,
-//            };
-//            var mockRepo = new Mock<ICategoryCommandRepository>();
-//            mockRepo.Setup(c => c.AddAsync(It.IsAny<Category>())).ReturnsAsync(true);
-//            var command = new CreateCategoryCommandHandler(mockRepo.Object);
+namespace FinalProject.Test.UnitTests.CategoryTests
+{
+    public class CreateCategoryCommandHandlerTest
+    {
+        [Fact]
+        public async void CreateCategory_IsSuccess()
+        {
+            CreateCategoryCommandRequest categoryRequest = new CreateCategoryCommandRequest()
+            {
+                Name = "TestCategory",
+                ShopListId = Guid.NewGuid(),
+            };
+            Category category = new Category()
+            {
+                Id = Guid.NewGuid(),
+                Name = "asdf",
+                ShopListId = categoryRequest.ShopListId,
+                CreationDate = DateTime.Now,
+                UpdateDate = DateTime.Now,
+            };
+            var mockRepo = new Mock<ICategoryCommandService>();
+            mockRepo.Setup(c => c.InsertAsync(It.IsAny<CategoryCreateDto>())).ReturnsAsync(new BaseResponse<CategoryCreateDto>(true));
+            var command = new CreateCategoryCommandHandler(mockRepo.Object);
 
-//            BaseResponse response = await command.Handle(categoryRequest, CancellationToken.None);
-//            Assert.True(response.Success);
-//            Assert.NotNull(response.Message);
-//        }
+            BaseResponse<CategoryCreateDto> response = await command.Handle(categoryRequest, CancellationToken.None);
+            Assert.True(response.Success);
+            Assert.NotNull(response.Message);
+        }
 
-//        [Fact]
-//        public async void CreateCategory_IsFailDataRequest()
-//        {
-//            CreateCategoryCommandRequest categoryRequest = new CreateCategoryCommandRequest()
-//            {
-//                Name = "TestCategory",
-//                ShopListId = Guid.NewGuid(),
-//            };
-//            Category category = new Category()
-//            {
-//                Id = Guid.NewGuid(),
-//                Name = "asdf",
-//                ShopListId = categoryRequest.ShopListId,
-//                CreationDate = DateTime.Now,
-//                UpdateDate = DateTime.Now,
-//            };
-//            var mockRepo = new Mock<ICategoryCommandRepository>();
-//            mockRepo.Setup(c => c.AddAsync(category)).ReturnsAsync(false);
-//            mockRepo.Setup(c => c.SaveAsync());
-//            var command = new CreateCategoryCommandHandler(mockRepo.Object);
+        [Fact]
+        public async void CreateCategory_IsFailDataRequest()
+        {
+            CreateCategoryCommandRequest categoryRequest = new CreateCategoryCommandRequest()
+            {
+                Name = "TestCategory",
+                ShopListId = Guid.NewGuid(),
+            };
+            Category category = new Category()
+            {
+                Id = Guid.NewGuid(),
+                Name = "asdf",
+                ShopListId = categoryRequest.ShopListId,
+                CreationDate = DateTime.Now,
+                UpdateDate = DateTime.Now,
+            };
+            var mockRepo = new Mock<ICategoryCommandService>();
+            mockRepo.Setup(c => c.InsertAsync(It.IsAny<CategoryCreateDto>())).ReturnsAsync(new BaseResponse<CategoryCreateDto>(false));
+            var command = new CreateCategoryCommandHandler(mockRepo.Object);
 
-//            BaseResponse response = await command.Handle(categoryRequest, CancellationToken.None);
-//            Assert.False(response.Success);
-//            Assert.Null(response.Message);
-//        }
-//    }
-//}
+            BaseResponse<CategoryCreateDto> response = await command.Handle(categoryRequest, CancellationToken.None);
+            Assert.False(response.Success);
+        }
+    }
+}
